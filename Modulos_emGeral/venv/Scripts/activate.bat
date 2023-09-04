4339a43cad406e7c3eb10fbc4d468b45e30b15c2
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-04 20:58:56 -0300

    recriando meu venv

--- Modulos_emGeral/venv/Scripts/activate.bat
+++ Modulos_emGeral/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\caio.dantas\Desktop\python_modules\Modulos_emGeral\venv
+set VIRTUAL_ENV=C:\Users\Caio\Desktop\python_modules\Modulos_emGeral\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
