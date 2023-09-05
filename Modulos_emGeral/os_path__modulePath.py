56c3aa20c92eb0a3844b2dc116d5d64a889ccdfb
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-05 16:29:23 -0300

    trabalhando com paths no module os

---
+++ Modulos_emGeral/os_path__modulePath.py
@@ -0,0 +1,40 @@
+# os.path trabalha com caminhos em Windows, Linux e Mac
+# Doc: https://docs.python.org/3/library/os.path.html#module-os.path
+# os.path é um módulo que fornece funções para trabalhar com caminhos de
+# arquivos em Windows, Mac ou Linux sem precisar se preocupar com as diferenças
+# entre esses sistemas.
+# Exemplos do os.path:
+# os.path.join: junta strings em um único caminho. Desse modo,
+# os.path.join('pasta1', 'pasta2', 'arquivo.txt') retornaria
+# 'pasta1/pasta2/arquivo.txt' no Linux ou Mac, e
+# 'pasta1\pasta2\arquivo.txt' no Windows.
+
+# os.path.split: divide um caminho em uma tupla (diretório, arquivo).
+# Por exemplo, os.path.split('/home/user/arquivo.txt')
+# retornaria ('/home/user', 'arquivo.txt').
+
+# os.path.exists: verifica se um caminho especificado existe.
+# os.path só trabalha com caminhos de arquivos e não faz nenhuma
+# operação de entrada/saída (I/O) com arquivos em si.
+import os
+
+CAMINHO = os.path.join('Desktop', 'python_module', 'python.py')
+print(CAMINHO)
+print()
+
+print(os.path.split(CAMINHO))
+print()
+
+repository, file = os.path.split(CAMINHO)
+print(repository, file)
+
+name_file, ext_file = os.path.splitext(file)
+print(name_file, ext_file)
+
+print(os.path.exists('C:\\Users\\caio.dantas'))
+print(os.path.abspath('.'))
+print(os.path.abspath('locale_traducoes.py'))
+print()
+print(os.path.basename(CAMINHO))
+print(os.path.basename(repository))
+
