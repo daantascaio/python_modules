ccbe1d5b3dc6c1774ef9aa0b685ed00c9cf7a876
daantascaio <caiodantasl.lara@gmail.com>
2023-09-13 01:20:49 -0300

    saving progess

--- os_shutil_copiandoANDcriandoFiles.py
+++ os_shutil_copiandoANDcriandoFiles.py
@@ -1,12 +1,10 @@
-# os + shutil - Mover, copiar e apagar arquvios
-# Mover/Renomear -> shutil.move
-# Mover/Renomear -> os.raneme
-# Copiar -> shutil.copy
-# Apagar -> os.unlink 
-# Apagar diretório recursivamente -> shutil.rmtree
-
 import os
+"""
 from pathlib import Path
+
+***
+# CÓDIGO CRIADO APENAS PARA FORNECER OS ARQUIVOS A SEREM MANIPULADOS NESSA AULA
+***
 
 # Obtém o diretório do arquivo em execução
 PATH_FILE = Path(__file__).parent.parent
@@ -27,4 +25,20 @@ for i in range(1, 6):
     with open(PATH_PASTA / nome_arquivo, 'w') as arquivo:
         arquivo.write("Este é um arquivo de exemplo.")
         # Você pode adicionar conteúdo ao arquivo aqui, se desejar
+"""
 
+# os + shutil - Copiando arquivos com Python
+# 1 - Vamos copiar arquivos de uma pasta para outra
+# Copiar -> shutil.copy
+
+import shutil 
+
+# Usando expeanduser('~') para trabalhar na HOME do usuário
+HOME = os.path.expanduser('~') 
+
+# Unindo o caminho do HOME com o diretory 'Desktop'. OBS: Desktop pode mudar de
+# nome, depende do OS do user.
+DESKTOP = os.path.join(HOME, 'Desktop')
+
+PASTA_ORIGINAL = os.path.join(DESKTOP, 'file_testing')
+print(HOME, DESKTOP, PASTA_ORIGINAL)
