6f9ab15ebc0fabc9a6229c10fe9ccbef50c4924f
Caio Dantas <caiodantasl.lara@gmail.com>
2023-09-21 16:35:11 -0300

    learning about selenium

--- Modulos_emGeral/SELENIUM_/make_chrome_browser_.py
+++ Modulos_emGeral/SELENIUM_/make_chrome_browser_.py
@@ -1,29 +1,18 @@
 # type: ignore
-"""from pathlib import Path
-
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-ROOT_FOLDER = Path(__file__).parent
-CHROMEDRIVER_EXEC = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
-
-chrome_options = webdriver.ChromeOptions()
-chrome_service = Service(executable_path=CHROMEDRIVER_EXEC)
-chrome_browser = webdriver.Chrome(
-    service=chrome_service,
-    options=chrome_options,
-)
-
-chrome_browser.get('https://www.google.com.br/')"""
 # Selenium - Automatizando tarefas no navegador
 from pathlib import Path
-# from time import sleep
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Chrome Options
 # https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
 
 
 # Caminho para a raiz do projeto
@@ -53,6 +42,8 @@ def make_chrome_browser(*options: str) -> webdriver.Chrome:
 
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
     # Example
     # options = '--headless', '--disable-gpu',
     options = ()
@@ -61,5 +52,13 @@ if __name__ == '__main__':
     # Como antes
     browser.get('https://www.google.com')
 
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+    search_input.send_keys('Hello World!')
+
     # Dorme por 10 segundos
-    # sleep(10)
+    sleep(TIME_TO_WAIT)
